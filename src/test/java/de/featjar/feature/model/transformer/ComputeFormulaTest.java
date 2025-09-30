4ca36bd08fadb1e02ff60a206c6e37840d0b2af3
Malena Horstmann <malena.horstmann@prime-force.com>
2025-09-30 15:19:23 +0200

    ComputeFormula first Unit Tests for only root and one feature

--- src/test/java/de/featjar/feature/model/transformer/ComputeFormulaTest.java
+++ src/test/java/de/featjar/feature/model/transformer/ComputeFormulaTest.java
@@ -1,16 +1,83 @@
 package de.featjar.feature.model.transformer;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import de.featjar.base.computation.Computations;
+import de.featjar.base.computation.ComputeConstant;
+import de.featjar.base.computation.Progress;
+import de.featjar.base.data.Result;
+import de.featjar.base.data.identifier.Identifiers;
+import de.featjar.feature.model.FeatureModel;
+import de.featjar.feature.model.FeatureTest;
+import de.featjar.feature.model.IFeature;
+import de.featjar.feature.model.IFeatureModel;
+import de.featjar.feature.model.IFeatureTree;
+import de.featjar.formula.structure.IFormula;
+import de.featjar.formula.structure.connective.And;
+import de.featjar.formula.structure.connective.BiImplies;
+import de.featjar.formula.structure.connective.Implies;
+import de.featjar.formula.structure.connective.Not;
+import de.featjar.formula.structure.connective.Or;
+import de.featjar.formula.structure.connective.Reference;
+import de.featjar.formula.structure.predicate.Literal;
+
 class ComputeFormulaTest {
-public static void main(String[] args) {
-	new ComputeFormulaTest().test();
-}
+	private IFeatureModel featureModel;
+	private IFormula expected;
+	
+    @BeforeEach
+    public void createFeatureModel() {
+        featureModel = new FeatureModel(Identifiers.newCounterIdentifier());
+        
+    }
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void onlyRoot() {
+		
+		// root and nothing else
+		featureModel.mutate().addFeatureTreeRoot(featureModel.mutate().addFeature("root"));
+		
+		// root must be selected
+		expected = new Reference(new And(new Literal("root")));
+		
+		executeTest();
+	}
+	
+	@Test
+	void oneFeature () {
+		
+		// root
+		IFeatureTree rootTree = 
+				featureModel.mutate().addFeatureTreeRoot(featureModel.mutate().addFeature("root"));
+		rootTree.mutate().toAndGroup();
+		
+		// TODO: check if setting root feature is missing here or if compute misses adding root feature literal
+
+		// create and add our only child
+		IFeature childFeature = featureModel.mutate().addFeature("Test1");
+		rootTree.mutate().addFeatureBelow(childFeature);
+		
+        expected = new Reference( new And (
+    				new Literal ("root"),
+        			new Implies( new Literal("Test1") , new Literal ("root") )
+        		));
+        
+        executeTest();
 	}
 
+	private void executeTest() {
+		
+		ComputeConstant<IFeatureModel> computeConstant = new ComputeConstant<IFeatureModel>(featureModel);
+		ComputeFormula computeFormula = new ComputeFormula(computeConstant);
+		
+		IFormula resultFormula = computeFormula.computeResult().get();
+		
+		// assert
+		assertEquals(expected, resultFormula);
+	}
 }
