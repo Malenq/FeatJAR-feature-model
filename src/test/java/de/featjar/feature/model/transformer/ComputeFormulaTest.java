e556c10f08dd2ac9d440b93a836ecfcb7f7273b4
Malena Horstmann <malena.horstmann@prime-force.com>
2025-09-30 11:02:03 +0200

    main method work around to debug cause tests in eclipse do not run

--- src/test/java/de/featjar/feature/model/transformer/ComputeFormulaTest.java
+++ src/test/java/de/featjar/feature/model/transformer/ComputeFormulaTest.java
@@ -5,7 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class ComputeFormulaTest {
-
+public static void main(String[] args) {
+	new ComputeFormulaTest().test();
+}
 	@Test
 	void test() {
 		fail("Not yet implemented");
