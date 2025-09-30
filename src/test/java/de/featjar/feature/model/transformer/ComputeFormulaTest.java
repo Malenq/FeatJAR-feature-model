d324f6eacf482bdb3f85c15811753207cfe72d0e
Malena Horstmann <malena.horstmann@prime-force.com>
2025-09-30 10:34:33 +0200

    testclass file push to check access rights

---
+++ src/test/java/de/featjar/feature/model/transformer/ComputeFormulaTest.java
@@ -0,0 +1,14 @@
+package de.featjar.feature.model.transformer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ComputeFormulaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
