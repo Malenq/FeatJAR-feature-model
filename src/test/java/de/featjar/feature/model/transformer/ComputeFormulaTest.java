b4893a09cc2f9d800c58f47c5c38084ca77a3944
Malena Horstmann <malena.horstmann@prime-force.com>
2025-09-30 16:19:23 +0200

    gradle spotless and license

--- src/test/java/de/featjar/feature/model/transformer/ComputeFormulaTest.java
+++ src/test/java/de/featjar/feature/model/transformer/ComputeFormulaTest.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright (C) 2025 FeatJAR-Development-Team
+ *
+ * This file is part of FeatJAR-feature-model.
+ *
+ * feature-model is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3.0 of the License,
+ * or (at your option) any later version.
+ *
+ * feature-model is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with feature-model. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * See <https://github.com/FeatureIDE/FeatJAR-feature-model> for further information.
+ */
 package de.featjar.feature.model.transformer;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 import de.featjar.base.computation.ComputeConstant;
 import de.featjar.base.data.identifier.Identifiers;
@@ -16,62 +33,58 @@ import de.featjar.formula.structure.connective.And;
 import de.featjar.formula.structure.connective.Implies;
 import de.featjar.formula.structure.connective.Reference;
 import de.featjar.formula.structure.predicate.Literal;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class ComputeFormulaTest {
-	private IFeatureModel featureModel;
-	private IFormula expected;
-	
+    private IFeatureModel featureModel;
+    private IFormula expected;
+
     @BeforeEach
     public void createFeatureModel() {
         featureModel = new FeatureModel(Identifiers.newCounterIdentifier());
-        
     }
-	
-	@Test
-	void onlyRoot() {
-		
-		// root and nothing else
-		featureModel.mutate().addFeatureTreeRoot(featureModel.mutate().addFeature("root"));
-		
-		// root must be selected
-		expected = new Reference(new And(
-					new Literal("root")
-				));
-		
-		executeTest();
-	}
-	
-	@Test
-	void oneFeature () {
-		
-		// root
-		IFeatureTree rootTree = 
-				featureModel.mutate().addFeatureTreeRoot(featureModel.mutate().addFeature("root"));
-		rootTree.mutate().toAndGroup();
-		
-		// TODO: check if setting root feature is missing here or if compute misses adding root feature literal
 
-		// create and add our only child
-		IFeature childFeature = featureModel.mutate().addFeature("Test1");
-		rootTree.mutate().addFeatureBelow(childFeature);
-		
-		// TODO: check order if bug is fixed
-        expected = new Reference( new And (
-    				new Literal ("root"),
-        			new Implies( new Literal("Test1") , new Literal ("root") )
-        		));
-        
+    @Test
+    void onlyRoot() {
+
+        // root and nothing else
+        featureModel.mutate().addFeatureTreeRoot(featureModel.mutate().addFeature("root"));
+
+        // root must be selected
+        expected = new Reference(new And(new Literal("root")));
+
         executeTest();
-	}
+    }
 
-	private void executeTest() {
-		
-		ComputeConstant<IFeatureModel> computeConstant = new ComputeConstant<IFeatureModel>(featureModel);
-		ComputeFormula computeFormula = new ComputeFormula(computeConstant);
-		
-		IFormula resultFormula = computeFormula.computeResult().get();
-		
-		// assert
-		assertEquals(expected, resultFormula);
-	}
+    @Test
+    void oneFeature() {
+
+        // root
+        IFeatureTree rootTree =
+                featureModel.mutate().addFeatureTreeRoot(featureModel.mutate().addFeature("root"));
+        rootTree.mutate().toAndGroup();
+
+        // TODO: check if setting root feature is missing here or if compute misses adding root feature literal
+
+        // create and add our only child
+        IFeature childFeature = featureModel.mutate().addFeature("Test1");
+        rootTree.mutate().addFeatureBelow(childFeature);
+
+        // TODO: check order if bug is fixed
+        expected = new Reference(new And(new Literal("root"), new Implies(new Literal("Test1"), new Literal("root"))));
+
+        executeTest();
+    }
+
+    private void executeTest() {
+
+        ComputeConstant<IFeatureModel> computeConstant = new ComputeConstant<IFeatureModel>(featureModel);
+        ComputeFormula computeFormula = new ComputeFormula(computeConstant);
+
+        IFormula resultFormula = computeFormula.computeResult().get();
+
+        // assert
+        assertEquals(expected, resultFormula);
+    }
 }
