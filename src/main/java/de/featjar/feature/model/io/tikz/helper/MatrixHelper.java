1ffcf20a9d3db89bbb4d459e902e9be74147d807
Lara <lara@M-Lara.local>
2025-10-10 11:38:21 +0200

    feat: added matrixhelper for cleaner code

--- src/main/java/de/featjar/feature/model/io/tikz/helper/MatrixHelper.java
+++ src/main/java/de/featjar/feature/model/io/tikz/helper/MatrixHelper.java
@@ -1,4 +1,58 @@
 package de.featjar.feature.model.io.tikz.helper;
 
+/**
+ * @author Felix Behme
+ * @author Lara Merza
+ * @author Jonas Hanke
+ */
 public class MatrixHelper {
+
+    private final static String NODE = "		\\node {replace}; \\\\" + System.lineSeparator();
+    private final static String DRAW = "			\\draw[drawColor] {replace};" + System.lineSeparator();
+    private final static String FILL_DRAW = " \\filldraw[drawColor] {replace}; " + System.lineSeparator();
+    private final static String FILL = "			\\fill[drawColor] {replace};" + System.lineSeparator();
+
+    private final StringBuilder stringBuilder;
+    private final String header ;
+
+    public MatrixHelper(MatrixType matrixType) {
+        this.stringBuilder = new StringBuilder();
+        this.header = matrixType.getHeader();
+        writeHeader();
+    }
+
+    private void writeHeader() {
+        stringBuilder.append(header);
+    }
+
+    private void writeFooter() {
+        stringBuilder.append("	};")
+                .append(System.lineSeparator());
+    }
+
+    public MatrixHelper writeNode(String value) {
+        stringBuilder.append(NODE.replace("{replace}", value));
+        return this;
+    }
+
+    public MatrixHelper writeDraw(String value) {
+        stringBuilder.append(DRAW.replace("{replace}", value));
+        return this;
+    }
+
+    public MatrixHelper writeFillDraw(String value) {
+        stringBuilder.append(FILL_DRAW.replace("{replace}", value));
+        return this;
+    }
+
+    public MatrixHelper writeFill(String value) {
+        stringBuilder.append(FILL.replace("{replace}", value));
+        return this;
+    }
+
+    public StringBuilder build() {
+        writeFooter();
+        return stringBuilder;
+    }
+
 }
--- src/main/java/de/featjar/feature/model/io/tikz/helper/MatrixType.java
+++ src/main/java/de/featjar/feature/model/io/tikz/helper/MatrixType.java
@@ -1,4 +1,25 @@
 package de.featjar.feature.model.io.tikz.helper;
 
+/**
+ * @author Felix Behme
+ * @author Lara Merza
+ * @author Jonas Hanke
+ */
 public enum MatrixType {
+
+    LEGEND("	\\matrix [anchor=north west] at (current bounding box.north east) {" + System.lineSeparator() + "		\\node [placeholder] {}; \\\\" + System.lineSeparator()
+            + "	};" + System.lineSeparator() + "	\\matrix [draw=drawColor,anchor=north west] at (current bounding box.north east) {" + System.lineSeparator()
+            + "		\\node [label=center:\\underline{Legend:}] {}; \\\\" + System.lineSeparator()),
+    CONSTRAINS(""),
+    ATTRIBUTES("");
+
+    final String header;
+
+    MatrixType(String header) {
+        this.header = header;
+    }
+
+    public String getHeader() {
+        return header;
+    }
 }
