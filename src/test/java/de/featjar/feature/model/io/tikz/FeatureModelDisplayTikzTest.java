48085914d1f43fe8bcc3146a913280e32cb2fc01
Lara <lara@M-Lara.local>
2025-10-16 14:58:22 +0200

    fix: correct feature model and added cardinality

--- src/test/java/de/featjar/feature/model/io/tikz/FeatureModelDisplayTikzTest.java
+++ src/test/java/de/featjar/feature/model/io/tikz/FeatureModelDisplayTikzTest.java
@@ -2,6 +2,7 @@ package de.featjar.feature.model.io.tikz;
 
 import de.featjar.base.FeatJAR;
 import de.featjar.base.data.Attribute;
+import de.featjar.base.data.Range;
 import de.featjar.base.data.identifier.Identifiers;
 import de.featjar.feature.model.*;
 import de.featjar.formula.structure.Expressions;
@@ -10,7 +11,6 @@ import de.featjar.formula.structure.term.value.Variable;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -35,33 +35,47 @@ public class FeatureModelDisplayTikzTest {
         FeatureModel featureModel = new FeatureModel(Identifiers.newCounterIdentifier());
 
         IFeature featureRootS = featureModel.mutate().addFeature("Hello");
+        IFeature feature = featureModel.mutate().addFeature("Feature");
+        IFeature world1 = featureModel.mutate().addFeature("World1");
+        IFeature world2 = featureModel.mutate().addFeature("World2");
+        IFeature wonderful1 = featureModel.mutate().addFeature("Wonderful1");
+        IFeature beautiful1 = featureModel.mutate().addFeature("Beautiful1");
+        IFeature wonderful2 = featureModel.mutate().addFeature("Wonderful2");
+        IFeature beautiful2 = featureModel.mutate().addFeature("Beautiful2");
+
         featureRootS.mutate().setAbstract();
-        // First Tree
+
+        // first tree
         IFeatureTree rootTree =
                 featureModel.mutate().addFeatureTreeRoot(featureRootS);
-        rootTree.mutate().toOrGroup();
+        rootTree.mutate().toAndGroup();
 
-        IFeature feature = featureModel.mutate().addFeature("Feature");
         IFeatureTree firstFeatureTree = rootTree.mutate().addFeatureBelow(feature);
-        firstFeatureTree.mutate().makeMandatory();
-        firstFeatureTree.mutate().toAlternativeGroup();
-
-        IFeature world = featureModel.mutate().addFeature("World");
-        rootTree.mutate().addFeatureBelow(world);
+        feature.mutate().setAbstract();
+        int group1 = firstFeatureTree.mutate().addAlternativeGroup();
+        int group2 = firstFeatureTree.mutate().addOrGroup();
 
         Attribute<String> attribute = new Attribute<>("any", "test", String.class);
         Attribute<String> attribute2 = new Attribute<>("anyd", "testd", String.class);
+        wonderful1.mutate().setAttributeValue(attribute, "value");
+        wonderful1.mutate().setAttributeValue(attribute2, "value2");
 
-        IFeature wonderful = featureModel.addFeature("Wonderful");
-        firstFeatureTree.mutate().addFeatureBelow(wonderful);
-        wonderful.mutate().setAttributeValue(attribute, "value");
-        wonderful.mutate().setAttributeValue(attribute2, "value2");
+        firstFeatureTree.mutate().addFeatureBelow(wonderful1, 0, group1);
 
-        IFeature beautiful = featureModel.addFeature("Beautiful");
-        firstFeatureTree.mutate().addFeatureBelow(beautiful);
+        firstFeatureTree.mutate().setFeatureCardinality(Range.of(0,2));
+        firstFeatureTree.mutate().addFeatureBelow(beautiful1, 1, group1);
+
+        firstFeatureTree.mutate().addFeatureBelow(wonderful2, 2, group2);
+        firstFeatureTree.mutate().addFeatureBelow(beautiful2, 3, group2);
+
+        rootTree.mutate().addFeatureBelow(world1);
+
+        IFeatureTree world2FeatureTree = rootTree.mutate().addFeatureBelow(world2);
+        world2FeatureTree.mutate().setFeatureCardinality(Range.of(1, 1));
+
+        // Constraints
         featureModel.addConstraint(new And(Expressions.literal("A"), Expressions.literal("B")));
         featureModel.addConstraint(new Implies(Expressions.literal("C"), new ForAll(new Variable("A"), new BiImplies(Expressions.literal("A"), Expressions.literal("C")))));
-
 
         FeatureModelDisplayTikzTest.featureModel = featureModel;
     }
