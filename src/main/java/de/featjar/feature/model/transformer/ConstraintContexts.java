12c113d79b5a1ef367ac1942aa904f7ac1168d73
Malena Horstmann <malena.horstmann@prime-force.com>
2025-10-17 16:23:32 +0200

    gradle spotless

--- src/main/java/de/featjar/feature/model/transformer/ConstraintContexts.java
+++ src/main/java/de/featjar/feature/model/transformer/ConstraintContexts.java
@@ -1,29 +1,27 @@
 package de.featjar.feature.model.transformer;
 
+import de.featjar.feature.model.IFeatureTree;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-import de.featjar.feature.model.IFeatureTree;
-
-
 /**
- * A class which can be used as a key in a HashMap which maps feature contexts to 
+ * A class which can be used as a key in a HashMap which maps feature contexts to
  * constraints.
- * 
+ *
  * @author Klara Surmeier
  * @author Nermine Mansour
  * @author Malena Horstmann
  */
 public class ConstraintContexts {
-	private final Set<IFeatureTree> contextFeatures;
+    private final Set<IFeatureTree> contextFeatures;
 
     public Set<IFeatureTree> getContextFeatures() {
-		return contextFeatures;
-	}
+        return contextFeatures;
+    }
 
-	public ConstraintContexts(Set<IFeatureTree> features) {
-        this.contextFeatures = new HashSet<>(features); 
+    public ConstraintContexts(Set<IFeatureTree> features) {
+        this.contextFeatures = new HashSet<>(features);
     }
 
     @Override
@@ -31,17 +29,13 @@ public class ConstraintContexts {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ConstraintContexts that = (ConstraintContexts) o;
-        
+
         return this.contextFeatures.equals(that.contextFeatures);
     }
 
     @Override
     public int hashCode() {
-      
+
         return Objects.hash(contextFeatures);
     }
 }
-
-
-
-
